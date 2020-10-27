753956ae30ccb39e8dc04a679f356029c934fa2d
leena0203 <leenasarode02@gmail.com>
2020-10-27 19:57:12 +0530

    Added: Method to upload CSV Files to perform Analysis

--- src/main/java/IPL_LeagueAnalysis/IPL_LeagueAnalysis.java
+++ src/main/java/IPL_LeagueAnalysis/IPL_LeagueAnalysis.java
@@ -15,37 +15,35 @@ public class IPL_LeagueAnalysis {
 	public static void main(String[] args) {
 		System.out.println("Welcome to IPL League Analysis");
 	}
-//	/**
-//	 * Load CSVFiles
-//	 * @param fileName
-//	 * @return
-//	 * @throws IOException
-//	 * @throws CSVBuilderExecption
-//	 */
-//	public int loadMostRunsCSVFile(String fileName) throws IOException, CSVBuilderExecption{
-//		Reader read = Files.newBufferedReader(Paths.get(fileName));
-//		ICSVBuilder csv = CSVBuilderFactory.createCSVBuilder();
-//		Iterator<IPLMostRuns> mostRunsIterator = csv.getCSVFileIterator(read, IPLMostRuns.class);
-//		int numOfRecord = this.getNumOfPlayers(mostRunsIterator);
-//		return numOfRecord;
-//	}
-//	public int loadMostWicketsCSVFile(String fileName) throws IOException, CSVBuilderExecption{
-//		Reader read = Files.newBufferedReader(Paths.get(fileName));
-//		ICSVBuilder csv = CSVBuilderFactory.createCSVBuilder();
-//		Iterator<IPLMostWickets> mostRunsIterator = csv.getCSVFileIterator(read, IPLMostWickets.class);
-//		int numOfRecord = this.getNumOfPlayers(mostRunsIterator);
-//		return numOfRecord;
-//	}
-//
-//
-//	private<E> int getNumOfPlayers(Iterator<E> iterator) {
-//		int numOfRecord = 0;
-//		while (iterator.hasNext()) {
-//			numOfRecord++;
-//			E censusData = iterator.next();
-//		}
-//		return numOfRecord;
-//	}
+	/**
+	 * Load CSVFiles
+	 * @param fileName
+	 * @return
+	 * @throws IOException
+	 * @throws CSVBuilderExecption
+	 */
+	public int loadMostRunsCSVFile(String fileName) throws IOException, CSVBuilderExecption{
+		Reader read = Files.newBufferedReader(Paths.get(fileName));
+		ICSVBuilder csv = CSVBuilderFactory.createCSVBuilder();
+		Iterator<IPLMostRuns> mostRunsIterator = csv.getCSVFileIterator(read, IPLMostRuns.class);
+		int numOfRecord = this.getNumOfPlayers(mostRunsIterator);
+		return numOfRecord;
+	}
+	public int loadMostWicketsCSVFile(String fileName) throws IOException, CSVBuilderExecption{
+		Reader read = Files.newBufferedReader(Paths.get(fileName));
+		ICSVBuilder csv = CSVBuilderFactory.createCSVBuilder();
+		Iterator<IPLMostWickets> mostRunsIterator = csv.getCSVFileIterator(read, IPLMostWickets.class);
+		int numOfRecord = this.getNumOfPlayers(mostRunsIterator);
+		return numOfRecord;
+	}
+	private<E> int getNumOfPlayers(Iterator<E> iterator) {
+		int numOfRecord = 0;
+		while (iterator.hasNext()) {
+			numOfRecord++;
+			E censusData = iterator.next();
+		}
+		return numOfRecord;
+	}
 
 
 }
--- src/test/java/IPL_LeagueAnalysis/IPL_LeagueAnalysisTest.java
+++ src/test/java/IPL_LeagueAnalysis/IPL_LeagueAnalysisTest.java
@@ -5,26 +5,27 @@ import org.junit.Test;
 
 
 public class IPL_LeagueAnalysisTest {
-//String FileName_MostRuns = "C:\\Users\\leena\\eclipse-workspace\\IPL_LeagueAnalysis\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
-//String FileName_MostWickets = "C:\\\\Users\\\\leena\\\\eclipse-workspace\\\\IPL_LeagueAnalysis\\\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
-//	@Test
-//	public void givenCSVFileOfRuns_ReturnsNumOfRecord() {
-//		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
-//		try {
-//			assertEquals(101, test.loadMostRunsCSVFile(FileName_MostRuns));
-//		}catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@Test
-//	public void givenCSVFileOfWickets_ReturnsNumOfRecord() {
-//		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
-//		try {
-//			assertEquals(99, test.loadMostWicketsCSVFile(FileName_MostWickets));
-//		}catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	String FileName_MostRuns = "C:\\Users\\leena\\eclipse-workspace\\IPL_LeagueAnalysis\\WP DP Data_01 IPL2019FactsheetMostRuns.csv";
+	String FileName_MostWickets = "C:\\\\Users\\\\leena\\\\eclipse-workspace\\\\IPL_LeagueAnalysis\\\\WP DP Data_02 IPL2019FactsheetMostWkts.csv";
+
+	@Test
+	public void givenCSVFileOfRuns_ReturnsNumOfRecord() {
+		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
+		try {
+			assertEquals(101, test.loadMostRunsCSVFile(FileName_MostRuns));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenCSVFileOfWickets_ReturnsNumOfRecord() {
+		IPL_LeagueAnalysis test = new IPL_LeagueAnalysis();
+		try {
+			assertEquals(99, test.loadMostWicketsCSVFile(FileName_MostWickets));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
